7fa93d740de9a3cb83e305a53b2ddf97592906c4
huongpx <huong.px01@gmail.com>
2023-06-05 01:06:35 +0700

    add lecturer model

---
+++ src/app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base_model import Base
+
+
+class Lecturer(Base):
+    __tablename__ = "lecturer"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255))
+    hashed_password: Mapped[str]
+    is_head: Mapped[bool] = mapped_column(default=False)
+    fullname: Mapped[str] = mapped_column(String(255))
