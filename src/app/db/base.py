017764fd48f4c452cd3f1052a4b978b972f668ce
huongpx <huong.px01@gmail.com>
2023-06-05 16:55:40 +0700

    update base model, change user -> lecturer

--- src/app/db/base.py
+++ src/app/db/base.py
@@ -2,4 +2,4 @@
 # and auto generate migration
 # https://stackoverflow.com/questions/15660676/alembic-autogenerate-producing-empty-migration
 from app.db.base_model import Base
-from app.models.user import Lecturer
+from app.models.lecturer import Lecturer
--- src/app/db/base_model.py
+++ src/app/db/base_model.py
@@ -1,5 +1,8 @@
+from typing import Any
+
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
+
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    id: Any
