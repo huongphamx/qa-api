959d4f9d9ab98e1d77e5e20dadef0512a9c296aa
huongpx <huong.px01@gmail.com>
2023-06-07 11:53:08 +0700

    update user -> lecturer endpoints

--- src/app/apis/__init__.py
+++ src/app/apis/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from .endpoints import auth, user
+from .endpoints import auth, lecturer
 
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(lecturer.router, prefix="/lecturers", tags=["lecturers"])
